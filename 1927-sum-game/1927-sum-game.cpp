38d4781b1ecde999931e11ce9083a8f6eda249c5
Aditya Sharma <adityasharma32001@gmail.com>
2026-08-23 12:19:06 +0530

    Time: 3 ms (66.67%), Space: 13.9 MB (67.83%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     bool sumGame(string num) {
-        const int n = num.length();
-        double ans = 0.0;
-
-        for (int i = 0; i < n / 2; ++i)
-        ans += getExpectation(num[i]);
-
-        for (int i = n / 2; i < n; ++i)
-        ans -= getExpectation(num[i]);
-
-        return ans != 0.0;
+        int n = num.length();
+        int cnt_1 = 0;
+        int cnt_2 = 0;
+        int sum = 0;
+        for(int i=0;i<n/2;i++){
+            if(num[i] != '?')sum += num[i]-'0';
+            else cnt_1++;
+        }
+        for(int i=n/2;i<n;i++){
+            if(num[i] != '?')sum -= num[i]-'0';
+            else cnt_2++;
+        }
+        if((cnt_1 + cnt_2) % 2 == 1)return true; 
+        
+        int ans = sum + (cnt_1/2)*9 - (cnt_2/2)*9;
+        return ans != 0;
+        
     }
-
-private:
-    double getExpectation(char c) {
-        return c == '?' ? 4.5 : c - '0';
-  }
 };
