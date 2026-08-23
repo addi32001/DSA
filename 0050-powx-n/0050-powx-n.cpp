2620c472a11e424bba78a8f20492b112989ae051
Aditya Sharma <adityasharma32001@gmail.com>
2026-08-23 11:48:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.35%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    double  power(double x, long n) {
-        // Base case: anything raised to 0 is 1
-        if (n == 0) return 1.0;
-
-        // Base case: anything raised to 1 is itself
-        if (n == 1) return x;
-
-        // If 'n' is even
-        if (n % 2 == 0) {
-            // Recursive call: x * x, n / 2
-            return power(x * x, n / 2);
+    double power(double x,long n){
+        if(n==0)return 1.0;
+        if(n==1)return x;
+        if(n%2 == 0){
+            return power(x*x , n/2);
+        }else{
+            return x * power(x,n-1);
         }
-        // If 'n' is odd
-        // Recursive call: x * power(x, n-1)
-        return x * power(x, n - 1);
     }
 
-public:
-    // Function to calculate x raised to n
     double myPow(double x, int n) {
-        // Store the value of n in a separate variable
-        long  num = n;
-
-        // If n is negative
-        if (num < 0) {
-            // Calculate the power of -n and take reciprocal
-            return (1.0 / power(x, -1 * num));
+        if(x == 1) return 1;
+        long nums = n;
+        if(n<0){
+            return (1.0/power(x,-1*nums));
         }
-        // If n is non-negative
-        return power(x, num);
+        return power(x,nums);
     }
 };
