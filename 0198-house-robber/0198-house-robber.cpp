49908f72c9c2974af4af0fbdb1a5a9b11c702e23
Aditya Sharma <adityasharma32001@gmail.com>
2026-09-22 23:23:49 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,7 +11,16 @@ public:
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return helper(nums.size()-1,nums,dp);
+        vector<int>dp(n,0);
+        //return helper(nums.size()-1,nums,dp);
+        dp[0] = nums[0];
+        if(n==1)return dp[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int take = nums[i] + dp[i-2];
+            int notTake = dp[i-1];
+            dp[i] = max(take, notTake);
+        }
+        return dp[n-1];
     }
 };
